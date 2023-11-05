f0146e0bfb16312dc639267d7e48b7e8fe5e8118
Dortiz246 <ortizdarryl@gmail.com>
2023-11-05 01:01:18 -0500

    opencv test file and working swarm

--- swarm_rabbit.py
+++ swarm_rabbit.py
@@ -1,22 +1,31 @@
 import os
 import pika
 from dotenv import load_dotenv
-
 load_dotenv()
-myIP = os.getenv('DARRYL_IP')
 
-main_machine_ip = os.getenv('ABED_IP')
-connection_params = pika.ConnectionParameters(host=main_machine_ip)
+DARRYL_IP = os.getenv("DARRYL_IP")
+ABED_IP = os.getenv("ABED_IP")
+RabbitUser = os.getenv("RabbitUser")
+RabbitPass = os.getenv("RabbitPass")
+RabbitVHOST = os.getenv("RabbitVHOST")
+RabbitQueue = os.getenv("RabbitQueue")
+
+connection_params = pika.ConnectionParameters(
+    host=ABED_IP,
+    virtual_host=RabbitVHOST,
+    credentials=pika.PlainCredentials(RabbitUser, RabbitPass)
+)
 
 connection = pika.BlockingConnection(connection_params)
 channel = connection.channel()
 
-channel.queue_declare(queue = os.getenv("RabbitQueue"), durable=True)
 
-message = "Hello from Sending Machine " +  str(myIP) + "!"
+channel.queue_declare(queue=RabbitQueue, durable=True)
+
+message = "Hello from Sending Machine " + DARRYL_IP + "!"
 channel.basic_publish(exchange='',
-                      routing_key = os.getenv('RabbitQueue'),
-                      body=message)
+                   routing_key=RabbitQueue,
+                   body=message)
 
 print(f"Sent: {message}")
 
